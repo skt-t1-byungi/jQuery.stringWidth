2b7e3ae8c5514a9d17c47a142cd23ac2cd36d262
jhs <sertion@innorix.com>
2016-09-02 18:44:49 +0900

    no message

--- src/jQuery.ellipsisWidth.js
+++ src/jQuery.ellipsisWidth.js
@@ -1,6 +1,36 @@
 var $ = require("jquery");
 var stringWidth = require("./stringWidth.js");
 
-$.fn.elipsisWidth = function() {
+var DEFAULT_OPTION = {
+    //newText: "블라블라..",
+    replace: '...',
+    position: 'after', //   front|middle|after
+    path: false,
+    pathSeparator: '/'
+};
+
+$.fn.elipsisWidth = function(param) {
+
+    //set option..
+    var option = {};
+
+    if (typeof param === 'number') {
+        $.extend(option, DEFAULT_OPTION, {
+            width: param
+        });
+    } else {
+        $.extend(option, DEFAULT_OPTION, param);
+    }
+
+    if (!option.width) {
+        option.width = this.width();
+    }
+
+    //text
+    var originText = option.newText || this.text();
+
+    //
+
+
 
 };
