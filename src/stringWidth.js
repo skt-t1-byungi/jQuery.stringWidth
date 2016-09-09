27da2efbaa25e722cf0064ce1525991549cad9c9
skt-t1-byungi <sertion@innorix.com>
2016-09-10 05:18:49 +0900

    fix err

--- src/stringWidth.js
+++ src/stringWidth.js
@@ -9,11 +9,11 @@ var context = document.createElement("canvas").getContext("2d");
 module.exports = function(el, str) {
 
     if (!el) {
-        new Error("stringWidth : required element param");
+        throw new Error("stringWidth : required element param");
     }
 
     if (!str) {
-        new Error("stringWidth : required string param");
+        throw new Error("stringWidth : required string param");
     }
 
     if (typeof str !== 'string' && !str.toString) {
